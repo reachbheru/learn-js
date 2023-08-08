5204ba149bbba83515f88412e8d3a18dc01417e9
reachbheru <reachbheru@gmail.com>
2023-08-08 12:10:35 +0530

    data type

--- dataTypes.js
+++ dataTypes.js
@@ -1,5 +1,6 @@
 //primitive data types are as follows-
 //Null,Undefined,Boolean,Number,BigInt,String,Symbol
+//symbol is unique
 //dynamic typing 
 let foo=5;//foo is now number
 foo=true;//foo is now boolean
@@ -7,7 +8,7 @@ foo="bar";//foo is now string
 
 //typeof use to give data types 
 let varNull=null;
-console.log(typeof varNull);//null is a object type
+console.log(typeof varNull);//null is a object type and a standalone value
 let varUndefined=undefined;
 console.log(varUndefined);//undefined is a undefined type
 
