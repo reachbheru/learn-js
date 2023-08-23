dd63620d86b74eeac644d72ab11f498a7fb199aa
reachbheru <reachbheru@gmail.com>
2023-08-23 18:18:04 +0530

    reverseString

---
+++ practice/basic_practice/reversing_a_string.js
@@ -0,0 +1,9 @@
+let originalString ="ship"
+let reverseString = " ";
+const len = originalString.length;
+for (let index = len-1; index >= 0; index--) {
+    reverseString += originalString[index];
+    
+}
+
+console.log(`reverse string is ${reverseString}`);
